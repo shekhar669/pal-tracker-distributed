26d658c91d707b9ea790c9cc72cf9f5045b1e61f
Ubuntu <ubuntu@ip-172-31-24-28.ap-southeast-1.compute.internal>
2020-12-11 15:11:52 +0000

    circuit braker

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,34 @@
 package io.pivotal.pal.tracker.allocations;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.client.RestOperations;
+import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
+    private Map<Long,ProjectInfo> cacheProjInfo= new ConcurrentHashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
-
+    @CircuitBreaker(name = "project", fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projInfo= restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        cacheProjInfo.put(projectId,projInfo);
+        return projInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId, Throwable cause) {
+        logger.info("Getting project with id {} from cache", projectId);
+        return cacheProjInfo.get(projectId);
     }
 }
+
