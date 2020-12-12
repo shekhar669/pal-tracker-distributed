a5e3feac07975ee38cafa1f8ebabfd77d536893c
Ubuntu <ubuntu@ip-172-31-24-28.ap-southeast-1.compute.internal>
2020-12-12 18:59:15 +0000

    updated oauth issue fix

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
@@ -10,7 +10,13 @@ import org.springframework.web.client.RestOperations;
 import java.util.TimeZone;
 
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
+@EnableWebSecurity
+@EnableResourceServer
+@EnableOAuth2Client
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
